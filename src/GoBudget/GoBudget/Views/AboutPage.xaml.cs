576ee77b9067a8adab29191e0623cc1e266c47b9
Ratn007 <ratn.kumar007@gmail.com>
2021-12-15 02:08:00 +0530

    1.0 Initial Project

---
+++ src/GoBudget/GoBudget/Views/AboutPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace GoBudget.Views
+{
+    public partial class AboutPage : ContentPage
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ src/GoBudget/GoBudget/Views/NewItemPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using GoBudget.Models;
+using GoBudget.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace GoBudget.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
