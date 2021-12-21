e2bca7ad4189981af6a849ce106c33c792dba0c0
Ratn007 <ratn.kumar007@gmail.com>
2021-12-21 18:36:01 +0530

    Xam101 - obeservale collections

--- src/GoBudget/GoBudget/ViewModels/HomeViewModel.cs
+++ src/GoBudget/GoBudget/ViewModels/HomeViewModel.cs
@@ -3,19 +3,31 @@ using MvvmHelpers.Commands;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 using System.Windows.Input;
 
 namespace GoBudget.ViewModels
 {
     public class HomeViewModel : ViewModelBase
     {
+        public ObservableRangeCollection<string> Coffee { get; }
         public HomeViewModel()
         {
             IncreaseCount = new Command(OnIncrease);
-            Title = "goedgo.com";
-        }
-        int count = 0;
+            Coffee = new ObservableRangeCollection<string>();
+            callServerCommand = new AsyncCommand(Callserver);
 
+            Title = "goedgo.com";
+
+        }
+        public ICommand callServerCommand { get; }
+        async Task Callserver()
+        {
+            var items = new List<string> { "yes plz", "tonx", "blue bottle" };
+            Coffee.AddRange(items);
+        }
+
+        int count = 0;
         public ICommand IncreaseCount { get; }
 
         private string _countDisplay = "Clicked  times!";
